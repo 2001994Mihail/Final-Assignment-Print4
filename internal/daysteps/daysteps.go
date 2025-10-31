e3b2eb9e3ca086e0e089f0015622bec750ca9c95
2001994Mihail <2001994mihail@users.noreply.github.com>
2025-10-31 09:04:40 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,20 +1,78 @@
 package daysteps
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
-	// Длина одного шага в метрах
-	stepLength = 0.65
-	// Количество метров в одном километре
-	mInKm = 1000
+	stepLengthCoefficient = 0.45
+	mInKm                 = 1000
 )
 
+// parsePackage парсит строку с данными о шагах и продолжительности
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	if data == "" {
+		return 0, 0, fmt.Errorf("empty input")
+	}
+
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("invalid format: expected 'steps,duration'")
+	}
+
+	// Парсим шаги - БЕЗ обрезки пробелов!
+	stepsStr := parts[0]
+	steps, err := strconv.Atoi(stepsStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid steps format: %v", err)
+	}
+
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("steps must be positive")
+	}
+
+	// Парсим продолжительность - БЕЗ обрезки пробелов!
+	durationStr := parts[1]
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid duration format: %v", err)
+	}
+
+	if duration <= 0 {
+		return 0, 0, fmt.Errorf("duration must be positive")
+	}
+
+	return steps, duration, nil
 }
 
+// DayActionInfo обрабатывает данные о дне и возвращает информацию о действии
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		// ЛОГИРОВАНИЕ ОШИБОК - добавлено
+		log.Println(err)
+		return ""
+	}
+
+	// Вычисляем дистанцию
+	stepLength := height * stepLengthCoefficient
+	distance := float64(steps) * stepLength / mInKm
+
+	// Вычисляем калории
+	kcal, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		// ЛОГИРОВАНИЕ ОШИБОК - добавлено
+		log.Println(err)
+		return ""
+	}
+
+	// УДАЛЕН завершающий символ новой строки
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		steps, distance, kcal)
 }
